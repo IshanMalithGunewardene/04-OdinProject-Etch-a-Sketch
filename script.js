1ef62123f423f3c81ff61b3ba77f1179498ade0e
ishan malith <ishanmalithgunewardene@gmail.com>
2025-03-11 08:19:38 +0530

    esketch is done

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello World!");
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(8, 1fr)";
-board.style.gridTemplateRows = "repeat(8, 1fr)";
-board.style.border = "1px solid red";
